757e2710a1d21ed0de7599f82db2db7b1f25e783
dhiazfathra <dhiazfathra@gmail.com>
2024-06-29 14:48:16 +0700

    refactor: add padding to the breadcrumbs

--- src/app/[locale]/page.tsx
+++ src/app/[locale]/page.tsx
@@ -3,7 +3,9 @@ import { FundDetails } from '@/templates/FundDetails';
 const GBSPage: React.FC = () => {
   return (
     <div className="container mx-auto p-4">
-      <div className="breadcrumbs text-sm">
+      <div className="breadcrumbs pb-4 text-sm">
+        {' '}
+        {/* Added pb-4 for padding bottom */}
         <ul className="flex items-center space-x-2">
           <li>
             <a href="#" className="text-gray-700 hover:text-blue-500">
